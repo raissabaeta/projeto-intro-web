f91f82a3b9f748a51b128e75dc27e257c0ec09c4
Raissa Baeta <rbaetadev@gmail.com>
2022-09-30 20:23:54 -0300

    Commit inicial semanas 1,2

---
+++ script.js
@@ -0,0 +1,324 @@
+
+let signos = []
+
+let caracteristicasSigno = ["Sincerona", "Independente", "Competitiva", "Cheia de energia"].toString()
+
+let signo1 = {
+    signo: "Áries",
+    nomeDaFamosa: "Anitta",
+    idadeDaFamosa: 29,
+    diaAniversario: "30 de março",
+    caracteristicasSigno: caracteristicasSigno,
+    verificaViva: true
+}
+
+
+caracteristicasSigno = ["Ciumenta", "Leal", "Prestativa", "Dedicada"].toString()
+
+let signo2 = {
+    signo: "Touro",
+    nomeDaFamosa: "Lizzo",
+    idadeDaFamosa: 34,
+    diaAniversario: "27 de abril",
+    caracteristicasSigno: caracteristicasSigno,
+    verificaViva: true
+}
+
+
+caracteristicasSigno = ["Divertida", "Comunicativa", "Boa ouvinte", "Insegura"].toString()
+
+let signo3 = {
+    signo: "Gêmeos",
+    nomeDaFamosa: "Angelina Jolie",
+    idadeDaFamosa: 47,
+    diaAniversario: "4 de junho",
+    caracteristicasSigno: caracteristicasSigno,
+    verificaViva: true
+}
+caracteristicasSigno = ["Romântica", "Sonhadora", "Fiel", "Dramática"].toString()
+
+let signo4 = {
+    signo: "Câncer",
+    nomeDaFamosa: "Princesa Diana",
+    idadeDaFamosa: 36,
+    diaAniversario: "1 de julho",
+    caracteristicasSigno: caracteristicasSigno,
+    verificaViva: false
+}
+caracteristicasSigno = ["Vaidosa", "Confiante", "Alegre", "Orgulhosa"].toString()
+
+let signo5 = {
+    signo: "Leão",
+    nomeDaFamosa: "Madonna",
+    idadeDaFamosa: 64,
+    diaAniversario: "16 de agosto",
+    caracteristicasSigno: caracteristicasSigno,
+    verificaViva: true
+}
+
+caracteristicasSigno = ["Inteligente", "Detalhista", "Discreta", "Organizada"].toString()
+
+let signo6 = {
+    signo: "Virgem",
+    nomeDaFamosa: "Beyoncé",
+    idadeDaFamosa: 41,
+    diaAniversario: "4 de setembro",
+    caracteristicasSigno: caracteristicasSigno, 
+    verificaViva: true
+}
+
+caracteristicasSigno = ["Indecisa", "Justiceira", "Sociável", "Empática"].toString()
+
+let signo7 = {
+    signo: "Libra",
+    nomeDaFamosa: "Pitty",
+    idadeDaFamosa: 44,
+    diaAniversario: "7 de outubro",
+    caracteristicasSigno: caracteristicasSigno,
+    verificaViva: true
+}
+
+caracteristicasSigno = ["Misteriosa", "Intensa", "Determinada", "Ambiciosa"].toString()
+
+let signo8 = {
+    signo: "Escorpião",
+    nomeDaFamosa: "Bruna Linzmeyer",
+    idadeDaFamosa: 29,
+    diaAniversario: "11 de novembro",
+    caracteristicasSigno: caracteristicasSigno,
+    verificaViva: true
+}
+
+caracteristicasSigno = ["Segura", "Inspiradora", "Curiosa", "Impaciente"].toString()
+
+let signo9 = {
+    signo: "Sagitário",
+    nomeDaFamosa: "Miley Cyrus",
+    idadeDaFamosa: 29,
+    diaAniversario: "23 de novembro",
+    caracteristicasSigno: caracteristicasSigno,
+    verificaViva: true
+}
+
+caracteristicasSigno = ["Disciplinada", "Focada", "Determinada", "Madura"].toString()
+
+let signo10 = {
+    signo: "Capricórnio",
+    nomeDaFamosa: "Rita Lee",
+    idadeDaFamosa: 74,
+    diaAniversario: "31 de dezembro",
+    caracteristicasSigno: caracteristicasSigno,
+    verificaViva: true
+}
+
+caracteristicasSigno = ["Criativa", "Teimosa", "Inventiva", "Exagerada"].toString()
+
+let signo11 = {
+    signo: "Aquário",
+    nomeDaFamosa: "Jennifer Aniston",
+    idadeDaFamosa: 53,
+    diaAniversario: "11 de fevereiro",
+    caracteristicasSigno: caracteristicasSigno,
+    verificaViva: true
+}
+
+caracteristicasSigno =  ["Sensível", "Generosa", "Calma", "Forte"].toString()
+
+let signo12 = {
+    signo: "Peixes",
+    nomeDaFamosa: "Marta",
+    idadeDaFamosa: 36,
+    diaAniversario: "19 de fevereiro",
+    caracteristicasSigno: caracteristicasSigno,
+    verificaViva: true
+}
+
+if(signo1.verificaViva === true){
+    signos.push(signo1)
+} else {
+    alert(`${signo1.nomeDaFamosa} não foi adicionada`)
+}
+
+if(signo2.verificaViva === true){
+    signos.push(signo2)
+} else {
+    alert(`${signo2.nomeDaFamosa} não foi adicionada`)
+}
+
+if(signo3.verificaViva === true){
+    signos.push(signo3)
+} else {
+    alert(`${signo3.nomeDaFamosa} não foi adicionada`)
+}
+
+if(signo4.verificaViva === true){
+    signos.push(signo4)
+} else {
+    alert(`${signo4.nomeDaFamosa} não foi adicionada`)
+}
+
+if(signo5.verificaViva === true){
+    signos.push(signo5)
+} else {
+    alert(`${signo5.nomeDaFamosa} não foi adicionada`)
+}
+
+if(signo6.verificaViva === true){
+    signos.push(signo6)
+} else {
+    alert(`${signo6.nomeDaFamosa} não foi adicionada`)
+}
+
+if(signo7.verificaViva === true){
+    signos.push(signo7)
+} else {
+    alert(`${signo7.nomeDaFamosa} não foi adicionada`)
+}
+
+if(signo8.verificaViva === true){
+    signos.push(signo8)
+} else {
+    alert(`${signo8.nomeDaFamosa} não foi adicionada`)
+}
+
+if(signo9.verificaViva === true){
+    signos.push(signo9)
+} else {
+    alert(`${signo9.nomeDaFamosa} não foi adicionada`)
+}
+
+if(signo10.verificaViva === true){
+    signos.push(signo10)
+} else {
+    alert(`${signo10.nomeDaFamosa} não foi adicionada`)
+}
+
+if(signo11.verificaViva === true){
+    signos.push(signo11)
+} else {
+    alert(`${signo11.nomeDaFamosa} não foi adicionada`)
+}
+
+if(signo12.verificaViva === true){
+    signos.push(signo12)
+} else {
+    alert(`${signo12.nomeDaFamosa} não foi adicionada`)
+}
+
+
+console.log(signos)
+
+for(signo of signos){
+    for(signoDaFamosa in signo){
+        console.log(`Signo: ${signo[signoDaFamosa]}`)
+    }
+    for(idadeDaFamosa in signo){
+        console.log(`idade: ${signo[idadeDaFamosa]}`)
+    }
+}
+
+// const calculaMediaIdade = Number(idadeDaFamosa1 + idadeDaFamosa2 + idadeDaFamosa3 +
+//     idadeDaFamosa4 + idadeDaFamosa5 + idadeDaFamosa6 + idadeDaFamosa7 + idadeDaFamosa8
+//     + idadeDaFamosa9 + idadeDaFamosa10 + idadeDaFamosa11 + idadeDaFamosa12) / 12;
+// console.log(calculaMediaIdade)
+
+// const verificaBooleano = verificaViva1 && verificaViva2 && verificaViva3 && verificaViva4 && verificaViva5
+//     && verificaViva6 && verificaViva7 && verificaViva8 && verificaViva9 && verificaViva10 && verificaViva11 && verificaViva12 === true;
+// console.log(verificaBooleano)
+
+
+// console.log(`${signo1.toUpperCase()} 
+// Nome da Famosa: ${nomeDaFamosa1}
+// Idade: ${idadeDaFamosa1}
+// Dia do aniversário: ${diaAniversario1}
+// Características do signo: ${caracteristicasSigno1}
+// Está viva?: ${verificaViva1}
+// `)
+
+// console.log(`${signo2.toUpperCase()} 
+// Nome da Famosa: ${nomeDaFamosa2}
+// Idade: ${idadeDaFamosa2}
+// Dia do aniversário: ${diaAniversario2}
+// Características do signo: ${caracteristicasSigno2}
+// Está viva?: ${verificaViva2}
+// `)
+
+// console.log(`${signo3.toUpperCase()} 
+// Nome da Famosa: ${nomeDaFamosa3}
+// Idade: ${idadeDaFamosa3}
+// Dia do aniversário: ${diaAniversario3}
+// Características do signo: ${caracteristicasSigno3}
+// Está viva?: ${verificaViva3}
+// `)
+
+// console.log(`${signo4.toUpperCase()} 
+// Nome da Famosa: ${nomeDaFamosa4}
+// Idade: ${idadeDaFamosa4}
+// Dia do aniversário: ${diaAniversario4}
+// Características do signo: ${caracteristicasSigno4}
+// Está viva?: ${verificaViva4}
+// `)
+
+// console.log(`${signo5.toUpperCase()} 
+// Nome da Famosa: ${nomeDaFamosa5}
+// Idade: ${idadeDaFamosa5}
+// Dia do aniversário: ${diaAniversario5}
+// Características do signo: ${caracteristicasSigno5}
+// Está viva?: ${verificaViva5}
+// `)
+
+// console.log(`${signo6.toUpperCase()} 
+// Nome da Famosa: ${nomeDaFamosa6}
+// Idade: ${idadeDaFamosa6}
+// Dia do aniversário: ${diaAniversario6}
+// Características do signo: ${caracteristicasSigno6}
+// Está viva?: ${verificaViva6}
+// `)
+
+// console.log(`${signo7.toUpperCase()} 
+// Nome da Famosa: ${nomeDaFamosa7}
+// Idade: ${idadeDaFamosa7}
+// Dia do aniversário: ${diaAniversario7}
+// Características do signo: ${caracteristicasSigno7}
+// Está viva?: ${verificaViva7}
+// `)
+
+// console.log(`${signo8.toUpperCase()} 
+// Nome da Famosa: ${nomeDaFamosa8}
+// Idade: ${idadeDaFamosa8}
+// Dia do aniversário: ${diaAniversario8}
+// Características do signo: ${caracteristicasSigno8}
+// Está viva?: ${verificaViva8}
+// `)
+
+// console.log(`${signo9.toUpperCase()} 
+// Nome da Famosa: ${nomeDaFamosa9}
+// Idade: ${idadeDaFamosa9}
+// Dia do aniversário: ${diaAniversario9}
+// Características do signo: ${caracteristicasSigno9}
+// Está viva?: ${verificaViva9}
+// `)
+
+// console.log(`${signo10.toUpperCase()} 
+// Nome da Famosa: ${nomeDaFamosa10}
+// Idade: ${idadeDaFamosa10}
+// Dia do aniversário: ${diaAniversario10}
+// Características do signo: ${caracteristicasSigno10}
+// Está viva?: ${verificaViva10}
+// `)
+
+// console.log(`${signo11.toUpperCase()} 
+// Nome da Famosa: ${nomeDaFamosa11}
+// Idade: ${idadeDaFamosa11}
+// Dia do aniversário: ${diaAniversario11}
+// Características do signo: ${caracteristicasSigno11}
+// Está viva?: ${verificaViva11}
+// `)
+
+// console.log(`${signo12.toUpperCase()} 
+// Nome da Famosa: ${nomeDaFamosa12}
+// Idade: ${idadeDaFamosa12}
+// Dia do aniversário: ${diaAniversario12}
+// Características do signo: ${caracteristicasSigno12}
+// Está viva?: ${verificaViva12}
+// `)
